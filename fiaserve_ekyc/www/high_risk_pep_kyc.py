4761dfb849b33e242c53c28648cd576e30d9acb2
Vickellah <magotovictor5@gmail.com>
2026-05-28 15:15:44 +0200

    Align KYC checklist fields and countries

--- fiaserve_ekyc/www/high_risk_pep_kyc.py
+++ fiaserve_ekyc/www/high_risk_pep_kyc.py
@@ -1,6 +1,8 @@
 import frappe
+from fiaserve_ekyc.fiaserve_ekyc.utils.seed_data import COUNTRIES
 
 
 def get_context(context):
 	context.no_cache = 1
 	context.title = "High Risk / PEP KYC - FIASERV eKYC"
+	context.countries = COUNTRIES
--- fiaserve_ekyc/www/non_individual_kyc.py
+++ fiaserve_ekyc/www/non_individual_kyc.py
@@ -1,6 +1,8 @@
 import frappe
+from fiaserve_ekyc.fiaserve_ekyc.utils.seed_data import COUNTRIES
 
 
 def get_context(context):
 	context.no_cache = 1
 	context.title = "Non-Individual KYC - FIASERV eKYC"
+	context.countries = COUNTRIES
