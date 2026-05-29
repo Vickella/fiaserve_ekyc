d40328ea3ab4803c0b761ae697fd95b617df6334
Vickellah <magotovictor5@gmail.com>
2026-05-29 10:28:00 +0200

    Resolve and seed country links during KYC submit

--- fiaserve_ekyc/fiaserve_ekyc/utils/kyc_submission.py
+++ fiaserve_ekyc/fiaserve_ekyc/utils/kyc_submission.py
@@ -1,6 +1,8 @@
 import frappe
 from frappe import _
 
+from fiaserve_ekyc.fiaserve_ekyc.utils.seed_data import ensure_country
+
 
 ALLOWED_DOCTYPES = {
 	"Individual Customer",
@@ -29,6 +31,7 @@ def submit_kyc(doctype, doc):
 		frappe.throw(_("Submitted document type does not match the selected KYC type."))
 
 	clean_doc = _clean_doc(doctype, doc)
+	_resolve_country_links(doctype, clean_doc)
 	created = frappe.get_doc(clean_doc)
 	created.insert(ignore_permissions=True)
 	frappe.db.commit()
@@ -67,6 +70,47 @@ def _clean_doc(doctype, doc):
 	for fieldname, value in doc.items():
 		if fieldname == "doctype" or fieldname not in allowed_fields:
 			continue
-		clean_doc[fieldname] = value
+		clean_doc[fieldname] = _clean_value(value)
 
 	return clean_doc
+
+
+def _clean_value(value):
+	if isinstance(value, str):
+		return " ".join(value.split())
+	if isinstance(value, list):
+		return [_clean_value(row) for row in value]
+	if isinstance(value, dict):
+		return {key: _clean_value(row_value) for key, row_value in value.items()}
+	return value
+
+
+def _resolve_country_links(doctype, doc):
+	meta = frappe.get_meta(doctype)
+	country_fields = {
+		field.fieldname
+		for field in meta.fields
+		if field.fieldtype == "Link" and field.options == "Country"
+	}
+
+	for fieldname in country_fields:
+		if doc.get(fieldname):
+			doc[fieldname] = ensure_country(doc[fieldname])
+
+	for field in meta.fields:
+		if field.fieldtype != "Table" or not doc.get(field.fieldname):
+			continue
+
+		child_meta = frappe.get_meta(field.options)
+		child_country_fields = {
+			child_field.fieldname
+			for child_field in child_meta.fields
+			if child_field.fieldtype == "Link" and child_field.options == "Country"
+		}
+		if not child_country_fields:
+			continue
+
+		for row in doc[field.fieldname]:
+			for child_fieldname in child_country_fields:
+				if row.get(child_fieldname):
+					row[child_fieldname] = ensure_country(row[child_fieldname])
